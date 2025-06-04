0d911393bbe90848eb8e3a4fc3fdb631bdb4d2bc
Michail Rontionov <m.rontionov@soton.ac.uk>
2025-06-04 21:07:30 +0100

    water

--- drink-reminder.py
+++ drink-reminder.py
@@ -3,8 +3,13 @@ import sys
 from picamera2 import Picamera2
 from PIL import Image
 import numpy as np
+import spidev
+import time
 
 picam2 = Picamera2()
+spi = spidev.SpiDev()
+
+WATER_LEVEL_CHANNEL = 0
 
 def init_sensors() -> bool:
     '''
@@ -15,6 +20,8 @@ def init_sensors() -> bool:
 
     pygame.mixer.init()
     picam2.start()
+    spi.open(0, 0)            # bus 0, device 0 (CE0)
+    spi.max_speed_hz = 1350000
     return True
 
 def play_audio(wav_file: str):
@@ -34,18 +41,29 @@ def save_image_np(img_np: np.ndarray, path: str) -> None:
     img = Image.fromarray(img_np)
     img.save(path)
 
+def read_analog_channel(channel):
+    # send start bit, single-ended mode, channel (ch 0–7)
+    command = 0b11 << 6 | (channel & 0x07) << 3
+    response = spi.xfer2([command, 0, 0])
+    return ((response[0] & 1) << 9) | (response[1] << 1) | (response[2] >> 7)
+
 # Execution begins here
 if not init_sensors():
     print(f'error: sensor initialisation failed')
     sys.exit(1)
 
-#play_audio('example.wav')
-img_np = capture_image_np(picam2)
-print(f"Image array shape: {img_np.shape}")
-save_image_np(img_np, './cam.png')
+# Interesting part begins here
 
-for channel in range(img_np.shape[2]):
-    print(f"Channel {channel} avg: {img_np[:,:,channel].mean()}")
+try:
+    while True:
+        water_level = read_analog_channel(WATER_LEVEL_CHANNEL)
+        print(water_level)
+        if (water_level >= 200):
+            play_audio('slurp.wav')
 
-if img_np[:,:,channel].mean() > 250:
-    play_audio('red.wav')
+        if (water_level <= 10):
+            play_audio('water.wav')
+
+        time.sleep(1)
+except KeyboardInterrupt:
+    print("Ctrl-C Pressed.")
