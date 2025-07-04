9fd65a2da531166b1e9a8eacc048e7227a58d6ea
Michail Rontionov <mrontionov@mront.io>
2025-07-04 20:03:47 +0100

    fixe reload_venv and loosen requirements.txt to only what we have

---
+++ import-test.py
@@ -0,0 +1,32 @@
+import spidev
+import time
+#from picamera2 import Picamera2 # has some unrelated to import issue
+from PIL import Image
+import numpy as np
+import RPi.GPIO as GPIO
+import time
+import adafruit_dht
+import board
+from signal import signal, SIGTERM, SIGHUP, pause
+from rpi_lcd import LCD
+import mpu6050
+import time
+import matplotlib.pyplot as plt
+import RPi.GPIO as GPIO
+import time
+import RPi.GPIO as GPIO
+import time
+import RPi.GPIO as GPIO
+import time
+import spidev
+import time
+from gpiozero import MotionSensor
+from time import sleep
+import RPi.GPIO as GPIO
+from gpiozero import DistanceSensor
+from time import sleep
+import matplotlib.pyplot as plt
+import spidev
+import time
+
+print("All import tests succeeded.")
--- pi-init/reload-venv.sh
+++ pi-init/reload-venv.sh
@@ -2,4 +2,7 @@
 
 VENV_PATH="$HOME/hackathon-venv"
 
-scripts/venv.sh
+scripts/venv.sh $VENV_PATH
+scripts/pip.sh $VENV_PATH
+
+printf "\n\nvenv reloaded. Either log in or log back our, or run\nsource $VENV_PATH/bin/activate\nto activate the new environment.\n"
